93610047a43cff8178d13e09d32da2a8c7eb0c4b
remi serougne <remiserougne@yahoo.fr>
2020-03-02 13:04:49 +0100

    homePage all*

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet,ScrollView, Text, View,Image } from 'react-native';
+import { SearchBar } from 'react-native-elements';
 
 export default function App() {
+  const [textSearch, setTextSearch] = useState("")
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+     <View style={{ flex: 1}}>
+       <View style={{ flexDirection:"row"}}>
+       <Image
+          style={{width: 40, height: 40, margin:5}}
+          source={require('./assets/logo.svg')}
+        />
+        <Text style={{ marginTop:15,marginLeft:5, fontSize:15, fontWeight:"500"}}>OrigApp</Text>
+       </View>
+        <View>
+          <SearchBar
+          placeholder="Recherche..."
+          onChangeText={(value)=> setTextSearch(value)}
+          value={textSearch}
+        />
+        </View>
+       
+            
+            <ScrollView style={{width:"100%"}}>
+            </ScrollView>
+      </View>
   );
 }
 
