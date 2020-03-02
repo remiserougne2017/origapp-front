5f81650d84043de30b44551858b2081c8cef74d7
remi serougne <remiserougne@yahoo.fr>
2020-03-02 18:43:52 +0100

    home & book

--- componentsNav/Home.js
+++ componentsNav/Home.js
@@ -1,12 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet,ScrollView, Text, View,Image } from 'react-native';
-import { SearchBar,Avatar, Badge, Icon, withBadge,Divider  } from 'react-native-elements'
+import { SearchBar,Avatar, Badge, Icon, withBadge,Divider  } from 'react-native-elements';
+import {Col} from 'reactstrap';
+import Book from './Books'
+import { set } from 'react-native-reanimated';
 
 export default function Home() {
-  const [textSearch, setTextSearch] = useState("")
+  const [textSearch, setTextSearch] = useState("");
+  const [cataList,setCataList]=useState([]);
+
+
+  useEffect(()=>{
+    const catalogue = async()=>{
+      var responseFetch = await fetch('http://10.2.5.203:3000/homePage')
+      var bookList = await responseFetch.json();
+      console.log(bookList)
+      setCataList(bookList)
+    };
+  },[])
+
+  //RS creation du tableau de books pour afficher le catalogue
+  var book = cataList.map((e,i)=>{
+   return(
+    <Books title={e.title} url={e.url} author={e.author} illustrator={e.illustrator} />
+   ) 
+
+  })
 
   return (
-     <View style={{ flex: 1}}>
+     <View style={{ flex: 1, width:"100%"}}>
        <View style={{ flexDirection:"row"}}>
        <Image
           style={{width: 40, height: 40, margin:5}}
@@ -28,13 +50,12 @@ export default function Home() {
           source={require('../assets/qr-scan.png')}
         />
         </View> 
-        <View name="Tags" style={{ flexDirection:"row",justifyContent:"left", alignItems:'center', marginTop:10, marginLeft:18}}>
-          <Badge value={<Text style={{color: 'white', paddingLeft:7,paddingRight:7,paddingTop:9, paddingBottom:12}}>Jeunesse</Text>}
-            badgeStyle={{backgroundColor:"grey"}}
-          />
-          <Badge value={<Text style={{color: 'white',  paddingLeft:7,paddingRight:7,paddingTop:9, paddingBottom:12}}>Histoire</Text>}
-            badgeStyle={{backgroundColor:"grey"}}
-          />
+        <View name="Tags" style={{ flexDirection:"row",justifyContent:"left", alignItems:'center', marginTop:10, marginLeft:18, width:"100%"}}>
+          <Col sm="3">
+            <Badge value={<Text style={{color: 'white', paddingLeft:7,paddingRight:7,paddingTop:9, paddingBottom:12}}>Jeunesse</Text>}
+              badgeStyle={{backgroundColor:"grey"}}
+            />
+          </Col>
         </View>
         <View  style={{ flexDirection:"row",justifyContent:"center", alignItems:'center'}}>
           <Divider style={{ backgroundColor: '#F9603E', width:"60%", opacity:"50%", marginTop:15}} />
@@ -47,6 +68,7 @@ export default function Home() {
         </View>
         
         <ScrollView style={{width:"100%"}}>
+          <Book/>
         </ScrollView>
       </View>
   );
