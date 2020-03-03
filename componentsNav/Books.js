ab2eef153fdadcad10111e4ea2a359fbe0f0c5bc
remi serougne <remiserougne@yahoo.fr>
2020-03-03 12:27:50 +0100

    kill boot

--- componentsNav/Books.js
+++ componentsNav/Books.js
@@ -1,28 +1,35 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { View, Text, Image } from 'react-native'
-import { Card, Icon,Rating  } from 'react-native-elements'
+import { Card, Icon,Rating,CheckBox  } from 'react-native-elements'
 // import {Col} from 'reactstrap';
 // import { Rating, AirbnbRating } from 'react-native-elements';
 
 const Book = (props) => {
-// implemented without image with header
+
+    const [isCheck, setIsCheck]=useState(false)
+
+    if(isCheck){
+
+    }else{
+
+    }
+
 return (
-    // <View style={{flex:1,flexDirection:"row"}}>
-    // <View style={{width:"40%", alignContent:"stretch", flexWrap:"wrap"}}>
             <Card containerStyle={{width:"45%",padding:2,backgroundColor:"white", marginLeft:"1%", marginRight:"1%"}}>
                 <Image
                     style={{width:"100%",height:250}}
                     resizeMode="cover"
                     source={{ uri: props.url }}
-                
                 />
-             <Icon containerStyle={{position: "absolute",
-                                    right: -10,
-                                    top: -15}}
-                name='ios-checkmark-circle'
-                type='ionicon'
-                color='#F9603E'
-                />
+                <CheckBox 
+                    onPress={() => setIsCheck(!isCheck)}
+                    checked={isCheck}
+                    checkedColor="#F9603E"
+                    containerStyle={{position: "absolute",
+                                    right: -30,
+                                    top: -23}}
+                    />
+
             <View style={{flexDirection:"column", alignItems:"flex-start", padding:10}}>
                 <Text>{props.editors}ed. Sabot Rouge 2020{props.years}</Text>
                 <Text>author illustrator</Text>
@@ -34,7 +41,6 @@ return (
                     />
             </View>
             </Card>
-        // </View>
       );
 };
 
