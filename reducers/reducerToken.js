2eefc0210c006c2399387c237685b8ca602c3428
Juliette Subra <juliette.subra@gmail.com>
2020-03-06 11:30:06 +0100

    ju reducers token et prenom logout

--- reducers/reducerToken.js
+++ reducers/reducerToken.js
@@ -2,7 +2,14 @@ export default function(token = 'dTsvaJw2PQiOtTWxykt5KcWco87eeSp6', action){
     if(action.type == 'addToken'){
         console.log('reducer')
         return action.token
+    } else if (action.type == 'deleteToken'){
+        console.log('reducer delete token');
+        let newToken = "";
+        token = newToken;
+        console.log('new token ?',token)
+        return token
     } else {
+
         return token
     }
 }
