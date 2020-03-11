57d3b7d236d26bf29eb93eb7aa8b4cdcd51a1f63
Juliette Subra <juliette.subra@gmail.com>
2020-03-11 10:38:26 +0100

    ip

--- componentsNav/Ip.js
+++ componentsNav/Ip.js
@@ -1,9 +1,5 @@
 const Ip = () => {
-<<<<<<< HEAD
-    return "http://10.2.5.203"
-=======
-    return "http://10.2.5.202"
->>>>>>> a8a7b05cd39358c769e110efd359cc39c7422e1c
+    return "http://10.2.3.37"
 }   
 
 export default Ip
